6e43048cd2c293c0eef20485079166aa529a334d
jelenakrsticjelena <jelllenakrstic@gmail.com>
2020-07-31 13:07:41 +0200

    30.07.2020

--- cypress/integration/create_gallery.spec.js
+++ cypress/integration/create_gallery.spec.js
@@ -3,7 +3,7 @@ import {authPage} from  '../page_object/login.page'
 import {createGallery} from '../page_object/gallery.page'
 
 let url1 = "https://gallery-app.vivifyideas.com/";
-let image1 = "https://jooinn.com/images/sunny-day-1.jpg";
+let image1 = "https://18ogess18pg1ptgub1nm316t-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/21333414_web1_200420-CRM-earth-day-EARTHDAY_1.jpg";
 let image2 = "https://www.e-know.ca/wp-content/uploads/2019/04/sunny.jpg";
 let image3 = "https://webcomicms.net/sites/default/files/clipart/172845/picture-sunny-day-172845-7790010.jpg"
 
@@ -35,22 +35,21 @@ describe('Create new gallery module', () => {
 
       it.only('GA-12 : Create New Gallery Page - provera paginacije', () => {
         
-        // for (var i = 0; i <= 10; i++) {
-        //   cy.visit('/create')
-        // createGallery.create('new gallery', 'description of new gallery', image1)
-        // }
-        // cy.visit('/my-galleries')
-        // cy.get('.grid').children().should('have.length', 10)
-        //createGalleryPage.create('new gallery','description of new gallery', image1)
-
-
-        // delete galleries
-       for (var i = 0; i < 10; i++) {
+        for (var i = 0; i <= 10; i++) {
+          cy.visit('/create')
+        createGallery.create('test gallery', 'description of test gallery', image1)
+        }
         cy.visit('/my-galleries')
-        cy.get('.box-title').eq(0).click()
-        cy.wait(1000)
-        cy.get('.btn').contains('Delete Gallery').click()
-      }
+        cy.get('.grid').children().should('have.length', 10)
+        // OVO NE   createGallery.create('new gallery','description of new gallery', image1)
+
+      // delete galleries
+      //  for (var i = 0; i < 10; i++) {
+      //   cy.visit('/my-galleries')
+      //   cy.get('.box-title').eq(0).click()
+      //   cy.wait(1000)
+      //   cy.get('.btn').contains('Delete Gallery').click()
+      // }
         
     })
      
